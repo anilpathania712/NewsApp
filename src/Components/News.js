a41290ad1ffc6be4573bfce5d0fb2bbffca8d2cf
anilpathania712 <anilpathania712@gmail.com>
2026-02-06 16:58:14 +0530

    Navbar News Category according

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Lottie from "lottie-react";
+import PropTypes from 'prop-types'
 
 export class News extends Component {
+  static defaultProps = {
+    country: 'us',
+    pageSize: 6,
+    category: 'general'
+  }
+
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string
+  }
 
     constructor(){
     super();
@@ -14,7 +26,7 @@ export class News extends Component {
   }
 
   async componentDidMount(){
-    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=0e88c67fb96243eca706518700500210&page=1&pageSize=${this.props.pageSize}`;
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=0e88c67fb96243eca706518700500210&page=1&pageSize=${this.props.pageSize}`;
     this.setState({loading:true});
     let data = await fetch(url);
     let parsedData = await data.json();
@@ -29,7 +41,7 @@ export class News extends Component {
    handleNextClick = async ()=>{
     console.log("Next");
     if(!(this.state.page + 1> Math.ceil(this.state.totalResults/this.props.pageSize))){
-    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=0e88c67fb96243eca706518700500210&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=0e88c67fb96243eca706518700500210&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
     this.setState({loading:true});
     let data = await fetch(url);
     let parsedData = await data.json();
@@ -44,7 +56,7 @@ export class News extends Component {
 
    handlePreviousClick = async ()=>{
     console.log("Previous");
-    let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=0e88c67fb96243eca706518700500210&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=0e88c67fb96243eca706518700500210&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
     this.setState({loading:true});
     let data = await fetch(url);
     let parsedData = await data.json();
@@ -59,6 +71,14 @@ export class News extends Component {
   render() {
     return (
       <div className='container my-3'>
+        <h2 className='text-center' style={{margin: '35px 0px'}}>NewsMonkey - Top Headlines</h2>
+        <div className="row">
+        {!this.state.loading && this.state.articles.map((element)=>{
+            return <div className="col-md-4" key={element.url} >
+            <NewsItem title={element.title?element.title.slice(0,50):""} description={element.description?element.description.slice(0,80):""} imgUrl ={element.urlToImage} newsUrl ={element.url}/>
+            </div>
+        })}
+
         {this.state.loading && (
         <div className="d-flex justify-content-center mx-10 my-3">
           <Lottie
@@ -69,14 +89,6 @@ export class News extends Component {
           />
         </div>
       )}
-
-        <h2 className='text-center'>NewsMonkey - Top Headlines</h2>
-        <div className="row">
-        {!this.state.loading && this.state.articles.map((element)=>{
-            return <div className="col-md-4" key={element.url} >
-            <NewsItem title={element.title?element.title.slice(0,50):""} description={element.description?element.description.slice(0,80):""} imgUrl ={element.urlToImage} newsUrl ={element.url}/>
-            </div>
-        })}
             
       </div>
       <div className="container d-flex justify-content-between">
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -6,7 +6,7 @@ export class MewsItem extends Component {
     let {title, description,imgUrl, newsUrl} = this.props;
     return (
       <div className='my-3'>
-        <div className="card" style={{width:"18rem"}}>
+        <div className="card">
           <img src={!imgUrl?"www.oregonlive.com/resizer/v2/6NCIE6LQFBH6PO64DU2BHKP4GI.JPG?auth=6f1846249a4ab91672ebe9f6eed79734c6311f0aa0ddf11db1638b88babe0182&width=1280&smart=true&quality=90":imgUrl} className="card-img-top" alt="..."/>
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
